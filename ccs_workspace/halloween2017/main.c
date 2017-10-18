4b25b0eead180d9efb054da03030f9a85b95d55d
George Louthan <georgerlouth@nthefourth.com>
2017-10-17 19:51:40 -0500

    Multiplex~!

--- ccs_workspace/halloween2017/main.c
+++ ccs_workspace/halloween2017/main.c
@@ -1,6 +1,7 @@
-#include "msp430fr2100.h"
+#include "msp430fr2111.h"
 #include "driverlib.h"
 #include <stdint.h>
+#include <stdlib.h>
 
 // Here's our Halloween setup:
 
@@ -9,6 +10,9 @@
 #define TX_PIN  BIT3
 #define KEY_PIN BIT4
 
+#define L_IN BIT0
+#define L_OUT BIT1
+
 /*
  * Here's our Halloween setup:
  *
@@ -16,6 +20,8 @@
  * 1.1 > CLK
  * 1.3 > TX
  * 1.4 < KEY
+ * 2.0 > L_IN  (to relay)
+ * 2.1 > L_OUT (to relay)
  *
  * And for the shift register:
  *
@@ -36,6 +42,18 @@
  *
  */
 
+volatile uint8_t f_second = 0;
+volatile uint8_t bulb=0;
+volatile uint16_t disp_num = 0;
+volatile uint8_t second_parts = 0;
+
+#define CLOCK_SPEED_KHZ 1000
+
+void delay_ms(uint16_t ms) {
+    while (ms--)
+        __delay_cycles(CLOCK_SPEED_KHZ);
+}
+
 void send_frame(uint16_t fr) {
     for (int i=0; i<16; i++) {
         // Set the output:
@@ -43,27 +61,41 @@ void send_frame(uint16_t fr) {
             P1OUT |= TX_PIN;
         else
             P1OUT &= ~TX_PIN;
-        __delay_cycles(100);
+//        __delay_cycles(CLOCK_SPEED_KHZ/500);
         // Pulse the clock:
         P1OUT |= CLK_PIN;
-        __delay_cycles(100);
+//        __delay_cycles(CLOCK_SPEED_KHZ/500);
         P1OUT &= ~CLK_PIN;
     }
     P1OUT &= ~NLE_PIN; // NLE is asserted low
-    __delay_cycles(100);
+//    __delay_cycles(CLOCK_SPEED_KHZ/500);
     P1OUT |= NLE_PIN; // back to idle high
-    __delay_cycles(100);
+//    __delay_cycles(CLOCK_SPEED_KHZ/500);
 }
 
 void send_digit(uint8_t digit, uint8_t tube) {
     uint16_t fr = 0b1111111111000000;
     fr |= (1 << tube);
-    fr |= 0b111;
     if (digit)
         fr &= ~(0x0001 << (5+digit));
     else
         fr &= ~(0x0001 << 15);
     send_frame(fr);
+}
+
+uint16_t ten_pow(uint8_t i) {
+    switch(i) {
+    case 0:
+        return 1;
+    case 1:
+        return 10;
+    case 2:
+        return 100;
+    case 3:
+        return 1000;
+    default:
+        return 0;
+    }
 }
 
 void main(void)
@@ -76,16 +108,27 @@ void main(void)
      */
     PM5CTL0 &= ~LOCKLPM5;
 
+    // Setup clocks:
+    // Default: MCLK and SMCLK using DCOCLKDIV at 1 MHz
+    //          ACLK uses REFO (~32k)
+    // This is fine.
+
     // NLE, CLK, TX are outputs:
     P1DIR |= NLE_PIN | CLK_PIN | TX_PIN;
     P1DIR &= ~KEY_PIN; // KEY is an input, external pull-whatever.
+
+    // L_IN and L_OUT are outputs:
+    P2DIR |= L_IN + L_OUT;
 
     // NLE and KEY are GPIO: (0b00)
     P1SEL1 &= ~(NLE_PIN | KEY_PIN);
     P1SEL0 &= ~(NLE_PIN | KEY_PIN);
     P1OUT |= NLE_PIN; // NLE is idle high.
 
-    // We will temporarily bit-bang.
+    // L_IN and L_OUT are GPIO, low:
+    P2SEL1 &= ~(L_IN + L_OUT);
+    P2SEL0 &= ~(L_IN + L_OUT);
+    P1OUT &= ~(L_IN + L_OUT);
 
     // CLK and TX
     P1SEL1 &= ~(CLK_PIN | TX_PIN);
@@ -93,12 +136,29 @@ void main(void)
 
     P1OUT &= ~(CLK_PIN | TX_PIN);
 
-    uint8_t dig=0;
+    Timer_B_initUpModeParam param = {0};
+    param.captureCompareInterruptEnable_CCR0_CCIE = TIMER_B_CCIE_CCR0_INTERRUPT_DISABLE;
+    param.clockSource = TIMER_B_CLOCKSOURCE_ACLK;
+    param.clockSourceDivider = TIMER_B_CLOCKSOURCE_DIVIDER_1;
+    param.startTimer = true;
+    param.timerClear = TIMER_B_DO_CLEAR;
+    param.timerInterruptEnable_TBIE = TIMER_B_TBIE_INTERRUPT_ENABLE; // TBIFG in TBxIV
+    param.timerPeriod = 32767 / 200; // 200 Hz
+
+    Timer_B_initUpMode(TIMER_B0_BASE, &param);
+
+    __bis_SR_register(LPM0_bits + GIE);
+
     while (1) {
-        send_digit(dig, 2);
-        dig = (dig+1) % 10;
-        __delay_cycles(1000000);
+        if (f_second) {
+            f_second = 0;
+
+            disp_num = (disp_num+1) % 1000;
+            P2OUT ^= L_IN;
+            P2OUT |= L_OUT;
+        }
     }
+
 
     // Old code:
     /*
@@ -112,10 +172,6 @@ void main(void)
     P1DIR |= BIT2|BIT5|BIT6|BIT7;
     P2DIR |= BIT0|BIT1|BIT6|BIT7;
 
-    // Setup clocks:
-    // Default: MCLK and SMCLK using DCOCLKDIV at 1 MHz
-    //          ACLK uses REFO (~32k)
-    // This is fine.
 
     // We'll set up in SPI master mode, with only MOSI and CLK in use.
     UCA0CTLW0 |= UCSWRST__ENABLE; // shut down for a mo (enable rst)
@@ -151,4 +207,19 @@ void main(void)
 
     while (1);
     */
+}
+
+#pragma vector=TIMER0_B1_VECTOR
+__interrupt void TIMERB0_ISR(void)
+{
+    if (TB0IV | TBIFG) {
+        send_digit((disp_num % ten_pow(bulb+1)) / ten_pow(bulb), bulb);
+        bulb = (bulb+1)%3;
+        second_parts++;
+        if (second_parts == 200) {
+            second_parts = 0;
+            f_second = 1;
+            __bic_SR_register_on_exit(LPM0_bits);
+        }
+    }
 }
